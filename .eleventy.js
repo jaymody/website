58cc95fd2ae7e9ff92beccf09cdbe58ef2bbf93d
jaymody <jaykmody@gmail.com>
2022-07-29 01:28:57 +0700

    Added support for image captions, mathjax, and footnotes via markdown-it.

---
+++ .eleventy.js
@@ -0,0 +1,12 @@
+module.exports = function (config) {
+  let md = require("markdown-it")({
+    html: true, // allows use of html in markdown ()
+    breaks: true // converts \n to <br>
+  });
+  md.use(require('markdown-it-implicit-figures'), { // makes images figures
+    figcaption: true // add figcaption using the alt text
+  });
+  md.use(require('markdown-it-footnote')); // use [^#] to create footnotes
+  md.use(require('markdown-it-mathjax')()); // render latex ($$...$$) with mathjax
+  config.setLibrary("md", md);
+}
